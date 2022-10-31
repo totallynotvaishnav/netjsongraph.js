e21d07d9b78ef5b4ddeff0c34b49e7449466891e
Vaishnav Nair <nairvaishnav007@icloud.com>
2022-10-31 23:56:31 +0530

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,20 @@ const path = require("path");
 const fs = require("fs");
 const TerserPlugin = require("terser-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 const examples = fs.readdirSync(
   path.resolve(__dirname, "public/example_templates"),
 );
-const templates = examples.map((example) => {
-  return new HtmlWebpackPlugin({
-    filename: `examples/${example}`,
-    inject: "head",
-    scriptLoading: "blocking",
-    template: path.resolve(__dirname, `public/example_templates/${example}`),
-  });
-});
+const templates = examples.map(
+  (example) =>
+    new HtmlWebpackPlugin({
+      filename: `examples/${example}`,
+      inject: "head",
+      scriptLoading: "blocking",
+      template: path.resolve(__dirname, `public/example_templates/${example}`),
+    }),
+);
 const minimizer = [
   new TerserPlugin({
     extractComments: false,
@@ -47,7 +49,26 @@ module.exports = (env, argv) => ({
     open: ["./index.html"],
     hot: true,
   },
-  plugins: [...templates],
+  plugins: [
+    ...templates,
+
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "public/assets"),
+          to: path.resolve(__dirname, "dist/public/assets"),
+        },
+        {
+          from: path.resolve(__dirname, "lib"),
+          to: path.resolve(__dirname, "dist/lib"),
+        },
+        {
+          from: path.resolve(__dirname, "src/css"),
+          to: path.resolve(__dirname, "dist/src"),
+        },
+      ],
+    }),
+  ],
   performance: {
     hints: false,
   },
